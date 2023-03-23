702534a560f8adea8649d7bc6afdbba04e813208
Sharfy <sharfy.adamantine@gmail.com>
2023-03-23 15:47:29 +0100

    Display outline layers on the map

--- web/src/components/Map/Map.tsx
+++ web/src/components/Map/Map.tsx
@@ -1,17 +1,35 @@
 import { useEffect, useState } from 'react'
 
-import { initializeMapboxConfig } from 'src/mapbox.config'
+import mapboxgl from 'mapbox-gl'
+
+import {
+  initializeMapboxConfig,
+  projectFillLayer,
+  projectOutlineLayer,
+} from 'src/mapbox.config'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 export const Map = () => {
+  const [map, setMap] = useState<mapboxgl.Map>()
   const [geoJson, setGeoJson] = useState()
+
   // Initialize Map
   useEffect(() => {
-    initializeMapboxConfig('map-container')
+    const initializedMap = initializeMapboxConfig('map-container')
+    setMap(initializedMap)
     fetchAndSetShapefiles(setGeoJson)
   }, [])
 
-  useEffect(() => {}, [geoJson])
+  useEffect(() => {
+    map.on('load', () => {
+      map.addSource('project', {
+        type: 'geojson',
+        data: geoJson,
+      })
+      map.addLayer(projectOutlineLayer('#00FF00'))
+      map.addLayer(projectFillLayer('#00FF00'))
+    })
+  }, [geoJson, map])
 
   return <div style={{ height: '100%', width: '100%' }} id="map-container" />
 }
--- web/src/mapbox.config.ts
+++ web/src/mapbox.config.ts
@@ -27,3 +27,23 @@ export const initializeMapboxConfig = (
     return map
   }
 }
+
+export const projectOutlineLayer = (lineColor: string) => ({
+  id: 'projectOutline',
+  type: 'line',
+  source: 'project',
+  paint: {
+    'line-color': lineColor,
+    'line-width': 3,
+  },
+})
+
+export const projectFillLayer = (lineColor: string) => ({
+  id: 'projectFill',
+  type: 'fill',
+  source: 'project', // reference the data source
+  paint: {
+    'fill-color': lineColor, // gainforest color fill
+    'fill-opacity': 0.05,
+  },
+})

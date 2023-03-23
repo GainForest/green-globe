46c3a8b13474250168e23d8614cbee9d6656add2
Sharfy <sharfy.adamantine@gmail.com>
2023-03-23 16:21:07 +0100

    Display geojson markers

--- web/src/components/Map/Map.tsx
+++ web/src/components/Map/Map.tsx
@@ -21,14 +21,27 @@ export const Map = () => {
   }, [])
 
   useEffect(() => {
-    map.on('load', () => {
-      map.addSource('project', {
-        type: 'geojson',
-        data: geoJson,
+    if (map && geoJson) {
+      map.on('load', () => {
+        map.addSource('project', {
+          type: 'geojson',
+          data: geoJson,
+        })
+        map.addLayer(projectOutlineLayer('#00FF00'))
+        map.addLayer(projectFillLayer('#00FF00'))
+        for (const feature of geoJson.features) {
+          console.log(geoJson)
+          // create a HTML element for each feature
+          const el = document.createElement('div')
+          el.className = 'map-marker'
+
+          // make a marker for each feature and add to the map
+          new mapboxgl.Marker(el)
+            .setLngLat(feature.geometry.coordinates[0][1])
+            .addTo(map)
+        }
       })
-      map.addLayer(projectOutlineLayer('#00FF00'))
-      map.addLayer(projectFillLayer('#00FF00'))
-    })
+    }
   }, [geoJson, map])
 
   return <div style={{ height: '100%', width: '100%' }} id="map-container" />

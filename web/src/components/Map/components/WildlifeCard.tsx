60eda6ce73623ef6534253abc433ca5671717679
Sharfy <sharfy.adamantine@gmail.com>
2023-03-27 14:50:14 +0200

    Start fetching the corrent wildlife video

--- web/src/components/Map/components/WildlifeCard.tsx
+++ web/src/components/Map/components/WildlifeCard.tsx
@@ -1,44 +1,71 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import { InfoBox } from './InfoBox'
 import { ToggleButton } from './ToggleButton'
 
 export const WildlifeCard = ({ activeFeature, activeProjectData }) => {
   const projectId = activeFeature?.properties?.projectId
-  const projectName = activeFeature?.properties?.name
-    ?.toLowerCase()
-    .split(' ')
-    .join('-')
-  const cameraPhoto =
+  const wildlifePhoto =
     activeProjectData?.project?.assets?.filter(
       (d) =>
         d.classification.includes('Camera Traps') && d.awsCID.includes('.jpg')
-    )?.[0] || ''
-  const cameraVideo =
+    )?.[0]?.awsCID || ''
+  const wildlifeVideo =
     activeProjectData?.project?.assets?.filter(
       (d) =>
         d.classification.includes('Camera Traps') && d.awsCID.includes('.mp4')
-    )?.[0] || ''
+    )?.[0]?.awsCID || ''
 
+  console.log(wildlifePhoto, wildlifeVideo, activeProjectData?.project?.assets)
   return (
     <InfoBox>
       <ToggleButton />
-      <img
-        alt="wildlife-camera"
-        src={`https://gainforest-transparency-dashboard.s3.amazonaws.com/camera-traps/${projectName}-1.jpg`}
-        style={{
-          width: '100%',
-          height: '280px',
-          objectFit: 'cover',
-          paddingTop: '20px',
-        }}
-      />
-      <p>
-        {projectName}
-        For more, visit the{' '}
-        <a href={`https://gainforest.app/data/${projectId}`}>
-          transparency dashboard
-        </a>
-        .
-      </p>
+      {wildlifePhoto.length ? (
+        <>
+          <img
+            alt="Wildlife camera still"
+            src={`${process.env.AWS_STORAGE}/${wildlifePhoto}`}
+            style={{
+              width: '100%',
+              height: '280px',
+              objectFit: 'cover',
+              paddingTop: '20px',
+            }}
+          />
+          <p>
+            {wildlifePhoto}
+            For more, visit the{' '}
+            <a href={`https://gainforest.app/data/${projectId}`}>
+              transparency dashboard
+            </a>
+            .
+          </p>
+        </>
+      ) : (
+        'This project has not uploaded a wildlife photo.'
+      )}
+      {wildlifeVideo.length ? (
+        <>
+          <video
+            src={`${process.env.AWS_STORAGE}/${wildlifeVideo}`}
+            style={{
+              width: '100%',
+              height: '280px',
+              objectFit: 'cover',
+              paddingTop: '20px',
+            }}
+          ></video>
+          <p>
+            {wildlifePhoto}
+            For more, visit the{' '}
+            <a href={`https://gainforest.app/data/${projectId}`}>
+              transparency dashboard
+            </a>
+            .
+          </p>
+        </>
+      ) : (
+        'This project has not uploaded a wildlife video.'
+      )}
     </InfoBox>
   )
 }

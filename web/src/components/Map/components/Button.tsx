b27ba5eec1df6fe546851fad1321f52512b2b331
Sharfy <sharfy.adamantine@gmail.com>
2023-04-07 10:54:25 +0200

    Add hover effect on the button

--- web/src/components/Map/components/Button.tsx
+++ web/src/components/Map/components/Button.tsx
@@ -1,4 +1,4 @@
-import { UnstyledButton } from './UnstyledButton'
+import styled from 'styled-components'
 
 export const Button = ({
   buttonIcon,
@@ -12,16 +12,7 @@ export const Button = ({
   onClick: () => void
 }) => {
   return (
-    <UnstyledButton
-      style={{
-        height: '44px',
-        width: '44px',
-        backgroundColor: active ? '#67962A' : '#ffffff',
-        bottom: 550,
-        left: position * 54 - 48,
-      }}
-      onClick={onClick}
-    >
+    <StyledButton active={active} position={position} onClick={onClick}>
       <span
         className="material-icons-round"
         style={{
@@ -32,6 +23,24 @@ export const Button = ({
       >
         {buttonIcon}
       </span>
-    </UnstyledButton>
+    </StyledButton>
   )
 }
+
+const StyledButton = styled.button<{
+  position: number
+  active: boolean
+}>`
+  position: absolute;
+  border-radius: 0.5em;
+  cursor: pointer;
+  height: 44px;
+  width: 44px;
+  bottom: 550px;
+  border: none;
+  left: ${(props) => `${props.position * 54 - 48}px`};
+  background-color: ${(props) => (props.active ? '#67962A' : '#ffffff')};
+  :hover {
+    background-color: ${(props) => (props.active ? '#67962A' : '#e9f5da')};
+  }
+`

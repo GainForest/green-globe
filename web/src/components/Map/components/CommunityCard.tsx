720dd8448789595ebf1919a0d9c29d10b8491643
Sharfy <sharfy.adamantine@gmail.com>
2023-04-07 12:26:55 +0200

    Svg splash for firstname and lastname

--- web/src/components/Map/components/CommunityCard.tsx
+++ web/src/components/Map/components/CommunityCard.tsx
@@ -45,33 +45,50 @@ export const CommunityCard = ({ activeProjectData }) => {
           Members of local communities who have received financial benefits from
           this project.
         </p>
-        {topFiveCommunityMembers.map((d) => (
-          <div style={{ marginTop: '32px' }} key={`community-member-${d.id}`}>
-            <div style={{ display: 'flex' }}>
-              <div>
-                <img
-                  alt={`${d.name}-profile`}
-                  src={d.profileUrl}
-                  width={100}
-                  height={100}
-                  style={{ borderRadius: 50 }}
-                />
+        {topFiveCommunityMembers.map((d) => {
+          let fullName
+          if (d.firstName && d.lastName) {
+            fullName = `${d.firstName} ${d.lastName}`
+          } else if (d.firstName && !d.lastName) {
+            fullName = `${d.firstName}`
+          } else if (!d.firstName && d.lastName) {
+            fullName = `${d.lastName}`
+          } else {
+            fullName = ''
+          }
+          const profileSrc =
+            d.profileUrl ||
+            `https://avatars.dicebear.com/api/initials/${fullName
+              .toLowerCase()
+              .replace(' ', '-')}.svg`
+          return (
+            <div style={{ marginTop: '32px' }} key={`community-member-${d.id}`}>
+              <div style={{ display: 'flex' }}>
+                <div>
+                  <img
+                    alt={`${d.name}-profile`}
+                    src={profileSrc}
+                    width={100}
+                    height={100}
+                    style={{ borderRadius: 50 }}
+                  />
+                </div>
+                <div style={{ marginLeft: '16px' }}>
+                  <h3>
+                    {d.firstName} {d.lastName}
+                  </h3>
+                  <p style={{ margin: 0, color: '#808080' }}>{d.role}</p>
+                  <p style={{ color: '#67962A' }}>
+                    {d.fundsReceived == 0
+                      ? 'No funds received.'
+                      : '$' + d.fundsReceived}
+                  </p>
+                </div>
               </div>
-              <div style={{ marginLeft: '16px' }}>
-                <h3>
-                  {d.firstName} {d.lastName}
-                </h3>
-                <p style={{ margin: 0, color: '#808080' }}>{d.role}</p>
-                <p style={{ color: '#67962A' }}>
-                  {d.fundsReceived == 0
-                    ? 'No funds received.'
-                    : '$' + d.fundsReceived}
-                </p>
-              </div>
+              <div style={{ marginTop: '12px' }}>{d.bio}</div>
             </div>
-            <div style={{ marginTop: '12px' }}>{d.bio}</div>
-          </div>
-        ))}
+          )
+        })}
         <button
           style={{
             border: 'none',

52f508b319d5f91e971e3eaa8bfd4d535b42f2b8
Sharfy <sharfy.adamantine@gmail.com>
2023-04-10 14:23:51 +0200

    Pixel push the close button

--- web/src/components/Map/components/ExitProjectView.tsx
+++ web/src/components/Map/components/ExitProjectView.tsx
@@ -12,13 +12,16 @@ export const ExitProjectView = ({
         height: '32px',
         width: '32px',
         left: 324,
-        bottom: 500,
+        bottom: 492,
         boxShadow:
           '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
       }}
       onClick={() => setDisplayBox(false)}
     >
-      <span style={{ fontSize: '20px' }} className="material-icons-round">
+      <span
+        style={{ fontSize: '20px', lineHeight: '32px' }}
+        className="material-icons-round"
+      >
         close
       </span>
     </UnstyledButton>

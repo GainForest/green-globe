c349253ea548e99609edb770f00abf8da1ca7a05
Sharfy <sharfy.adamantine@gmail.com>
2023-04-10 14:18:05 +0200

    Change the 'exit project' button with a close button

--- web/src/components/Map/components/ExitProjectView.tsx
+++ web/src/components/Map/components/ExitProjectView.tsx
@@ -8,14 +8,19 @@ export const ExitProjectView = ({
   return (
     <UnstyledButton
       style={{
-        height: '48px',
-        width: '160px',
-        right: 80,
-        top: 60,
+        zIndex: 2,
+        height: '32px',
+        width: '32px',
+        left: 324,
+        bottom: 500,
+        boxShadow:
+          '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
       }}
       onClick={() => setDisplayBox(false)}
     >
-      <p style={{ lineHeight: '40px', margin: 0 }}>Exit Project View</p>
+      <span style={{ fontSize: '20px' }} className="material-icons-round">
+        close
+      </span>
     </UnstyledButton>
   )
 }

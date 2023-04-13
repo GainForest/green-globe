c318613cb47e67e74db4e1c20e0aba7c100f7c1e
Sharfy <sharfy.adamantine@gmail.com>
2023-04-13 14:22:41 +0200

    Forgot to add ProjectSeriesPickerOverlay

---
+++ web/src/components/Map/components/ProjectSeriesPickerOverlay.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+
+import { addMarkers } from '../maputils'
+
+export const ProjectSeriesPickerOverlay = ({
+  map,
+  markers,
+  projectPolygons,
+  verraPolygons,
+  setActiveProjectPolygon,
+  setActiveProject,
+  setDisplayOverlay,
+  setMarkers,
+}) => {
+  const [gainForestDisplayed, setGainForestDisplayed] = useState<boolean>(true)
+  const [verraDisplayed, setVerraDisplayed] = useState<boolean>(false)
+
+  return (
+    <div
+      style={{
+        boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+        cursor: 'pointer',
+        width: '220px',
+        height: '60px',
+        backgroundColor: '#ffffff',
+        position: 'absolute',
+        bottom: 210,
+        right: 8,
+        borderRadius: '8px',
+        padding: '4px 8px',
+      }}
+    >
+      <p style={{ fontSize: '0.75rem' }}>
+        <input
+          defaultChecked
+          type="checkbox"
+          onClick={() => {
+            if (gainForestDisplayed) {
+              markers.forEach((marker) => {
+                if (marker._element.className.includes('gainforest')) {
+                  marker.remove()
+                }
+              })
+            } else {
+              const newMarkers = addMarkers(
+                map,
+                projectPolygons,
+                'gainforest',
+                setActiveProjectPolygon,
+                setActiveProject,
+                setDisplayOverlay
+              )
+              setMarkers([...markers, ...newMarkers])
+            }
+            setGainForestDisplayed(!gainForestDisplayed)
+          }}
+        />
+        Display GainForest Projects
+      </p>
+      <p style={{ fontSize: '0.75rem' }}>
+        <input
+          type="checkbox"
+          onClick={() => {
+            if (verraDisplayed) {
+              markers.forEach((marker) => {
+                if (marker._element.className.includes('verra')) {
+                  marker.remove()
+                }
+              })
+            } else {
+              const newMarkers = addMarkers(
+                map,
+                verraPolygons,
+                'verra',
+                setActiveProjectPolygon,
+                setActiveProject,
+                setDisplayOverlay
+              )
+              setMarkers([...markers, ...newMarkers])
+            }
+            setVerraDisplayed(!verraDisplayed)
+          }}
+        />
+        Display Verra Projects
+      </p>
+    </div>
+  )
+}

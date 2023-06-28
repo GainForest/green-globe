ed66d753aa8c3edbb1c182fe0e575a4648f24600
Sharfy <sharfy.adamantine@gmail.com>
2023-06-28 15:40:43 +0200

    Add asset type

---
+++ web/src/types.d.ts
@@ -0,0 +1,21 @@
+// Not the complete interface
+
+interface Asset {
+  id: string
+  name: string
+  uploadedById: number
+  lastUpdatedById?: number
+  projectId?: number
+  classification: string
+  ipfsCID: string
+  description?: string
+  type: string
+  extension?: string
+  geoInformation?: any //not sure about this one
+  awsCID?: string
+  isArchived: boolean
+  shapefile: {
+    default: boolean
+    shortName
+  }
+}

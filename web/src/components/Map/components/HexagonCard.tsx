c2331ecf3df4d2ad9e23dd6f16b121f31a5b642d
Sharfy <sharfy.adamantine@gmail.com>
2023-07-04 14:07:58 +0200

    Create add plot to my ecosystems

--- web/src/components/Map/components/HexagonCard.tsx
+++ web/src/components/Map/components/HexagonCard.tsx
@@ -20,16 +20,39 @@ export const HexagonCard = ({ clickedCoords }) => {
 
   return (
     <InfoBox>
-      <br />
-      <p style={{ color: '#FFFFFF' }}>{whatThreeWords}</p>
-
-      <video
-        width="200"
-        height="200"
-        autoPlay
-        loop
-        src="https://gainforest-transparency-dashboard.s3.amazonaws.com/nftree-video/defensores-del-chaco-video.mp4"
-      />
+      <div style={{ display: 'flex', flexDirection: 'column', margin: '12px' }}>
+        <p style={{ color: '#000000' }}>
+          <span style={{ color: '#E11F26' }}>{'/// '}</span>
+          {whatThreeWords}
+        </p>
+        <div
+          style={{
+            height: '250px',
+            width: '250px',
+            border: '1px solid #000000',
+            backgroundColor: '#F5F7F9',
+            clipPath:
+              'polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%)',
+            margin: '0 auto',
+          }}
+        >
+          <video
+            style={{
+              textAlign: 'center',
+              position: 'relative',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+            width="170"
+            height="170"
+            autoPlay
+            loop
+            src="https://gainforest-transparency-dashboard.s3.amazonaws.com/nftree-video/defensores-del-chaco-video.mp4"
+          />
+        </div>
+        <button>Add plot to my ecosytems</button>
+      </div>
     </InfoBox>
   )
 }

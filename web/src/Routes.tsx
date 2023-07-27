fa8999cd646cdf510419ee9be59b35f7db320004
Sharfy <sharfy.adamantine@gmail.com>
2023-07-27 11:48:48 +0200

    Remove ugly login page route

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,16 +9,17 @@
 
 import { Router, Route, Set } from '@redwoodjs/router'
 
-import { useAuth } from './auth'
+// import { useAuth } from './auth'
 
 const Routes = () => {
   return (
-    <Router useAuth={useAuth}>
+    <Router>
       <Route path="/login" page={AuthPage} name="login" />
-      <Set private unauthenticated="login">
-        <Route path="/" page={LandingPage} name="landing" />
-        <Route path="/app/{urlProjectId:Int}" page={MapPage} name="map" />
-        <Route path="/app" page={MapPage} name="map" />
+      <Set>
+        {/* <Set private unauthenticated="login"> */}
+        {/* <Route path="/" page={LandingPage} name="landing" /> */}
+        <Route path="/{urlProjectId:Int}" page={MapPage} name="map" />
+        <Route path="/" page={MapPage} name="map" />
         <Route notfound page={NotFoundPage} />
       </Set>
     </Router>

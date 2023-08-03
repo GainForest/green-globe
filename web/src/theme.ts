3a8845f04459f3239421e503724bdc5c1e8dfe02
Sharfy <sharfy.adamantine@gmail.com>
2023-08-03 10:49:05 +0200

    Fix the colors again

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -8,8 +8,9 @@ export const theme: Theme = {
   },
   colors: {
     text: '#ebebec',
-    background: '#F9EFE4',
-    secondaryBackground: '#F9EFE4', // for dropdowns, modals
+    background: '#22252A',
+    secondaryBackground: '#1E2024', // for dropdowns, modals
+    tertiaryBackground: '#272A2F',
     primary: '#ebebec',
     secondary: '#808080',
     muted: '#393a3d',
@@ -17,33 +18,33 @@ export const theme: Theme = {
     skeletonHighlight: '#393a3d',
     boxShadow: '#000000',
     green: '#67962A',
-    modes: {
-      // light: {
-      //   text: '#1b1e23',
-      //   background: '#FFFFFF',
-      //   secondaryBackground: '#f5f5f5', // for dropdowns, modals
-      //   tertiaryBackground: '#E8EAED',
-      //   primary: '#000000',
-      //   secondary: '#808080',
-      //   muted: '#b9b9bb',
-      //   hinted: '#f2f2f1',
-      //   skeletonHighlight: '#f5f5f5',
-      //   boxShadow: '#b9b9bb',
-      //   green: '#67962A',
-      // },
-      dark: {
-        text: '#ebebec',
-        background: '#22252A',
-        secondaryBackground: '#1E2024', // for dropdowns, modals
-        tertiaryBackground: '#272A2F',
-        primary: '#ebebec',
-        secondary: '#808080',
-        muted: '#393a3d',
-        hinted: '#2a2c30',
-        skeletonHighlight: '#393a3d',
-        boxShadow: '#000000',
-        green: '#67962A',
-      },
-    },
+    // modes: {
+    // light: {
+    //   text: '#1b1e23',
+    //   background: '#FFFFFF',
+    //   secondaryBackground: '#f5f5f5', // for dropdowns, modals
+    //   tertiaryBackground: '#E8EAED',
+    //   primary: '#000000',
+    //   secondary: '#808080',
+    //   muted: '#b9b9bb',
+    //   hinted: '#f2f2f1',
+    //   skeletonHighlight: '#f5f5f5',
+    //   boxShadow: '#b9b9bb',
+    //   green: '#67962A',
+    // },
+    // dark: {
+    //   text: '#ebebec',
+    //   background: '#22252A',
+    //   secondaryBackground: '#1E2024', // for dropdowns, modals
+    //   tertiaryBackground: '#272A2F',
+    //   primary: '#ebebec',
+    //   secondary: '#808080',
+    //   muted: '#393a3d',
+    //   hinted: '#2a2c30',
+    //   skeletonHighlight: '#393a3d',
+    //   boxShadow: '#000000',
+    //   green: '#67962A',
+    // },
+    // },
   },
 }

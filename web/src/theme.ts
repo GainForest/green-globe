c17f050296fd7fa1d8e371b6c5139bfb87726216
Sharfy <sharfy.adamantine@gmail.com>
2023-08-03 10:44:44 +0200

    Comment out light mode

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -18,19 +18,19 @@ export const theme: Theme = {
     boxShadow: '#000000',
     green: '#67962A',
     modes: {
-      light: {
-        text: '#1b1e23',
-        background: '#FFFFFF',
-        secondaryBackground: '#f5f5f5', // for dropdowns, modals
-        tertiaryBackground: '#E8EAED',
-        primary: '#000000',
-        secondary: '#808080',
-        muted: '#b9b9bb',
-        hinted: '#f2f2f1',
-        skeletonHighlight: '#f5f5f5',
-        boxShadow: '#b9b9bb',
-        green: '#67962A',
-      },
+      // light: {
+      //   text: '#1b1e23',
+      //   background: '#FFFFFF',
+      //   secondaryBackground: '#f5f5f5', // for dropdowns, modals
+      //   tertiaryBackground: '#E8EAED',
+      //   primary: '#000000',
+      //   secondary: '#808080',
+      //   muted: '#b9b9bb',
+      //   hinted: '#f2f2f1',
+      //   skeletonHighlight: '#f5f5f5',
+      //   boxShadow: '#b9b9bb',
+      //   green: '#67962A',
+      // },
       dark: {
         text: '#ebebec',
         background: '#22252A',

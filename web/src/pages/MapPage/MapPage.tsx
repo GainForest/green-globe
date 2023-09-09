0c7a8a19d862c41501154754c9065870a30ef4d6
Sharfy <sharfy.adamantine@gmail.com>
2023-09-09 22:48:52 +0200

    Move the useEffect out

--- web/src/pages/MapPage/MapPage.tsx
+++ web/src/pages/MapPage/MapPage.tsx
@@ -11,26 +11,25 @@ const MapPage = ({ urlProjectId }) => {
   const { isAuthenticated } = useAuth()
 
   // Check user-agent string if the user is on mobile but not on ipad.
-  useEffect(() => {
-    const mobileRegex = /\b(Android|webOS|iPhone|BlackBerry|Windows Phone)\b/
-    if (mobileRegex.test(navigator.userAgent)) {
-      return (
-        <>
-          <MetaTags title="Home" description="Explore GainForest Projects" />
+  const mobileRegex = /\b(Android|webOS|iPhone|BlackBerry|Windows Phone)\b/
 
-          <div style={{ width: '100vw', height: '100vh' }}>
-            <Navbar isAuthenticated={isAuthenticated} />
-            <h1>
-              Mobile is not supported yet. Please visit{' '}
-              <a href="https://www.data.gainforest.app">
-                https://www.data.gainforest.app
-              </a>
-            </h1>
-          </div>
-        </>
-      )
-    }
-  }, [])
+  if (mobileRegex.test(navigator.userAgent)) {
+    return (
+      <>
+        <MetaTags title="Home" description="Explore GainForest Projects" />
+
+        <div style={{ width: '100vw', height: '100vh' }}>
+          <Navbar isAuthenticated={isAuthenticated} />
+          <h1>
+            Mobile is not supported yet. Please visit{' '}
+            <a href="https://www.data.gainforest.app">
+              https://www.data.gainforest.app
+            </a>
+          </h1>
+        </div>
+      </>
+    )
+  }
 
   return (
     <>

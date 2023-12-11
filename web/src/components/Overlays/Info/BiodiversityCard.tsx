fe670812d2e172a97b55ac0c696faeff91079830
Zach Lefler <zachary.lefler@gmail.com>
2023-12-11 10:11:13 -0800

    fetch trees, parse data

--- web/src/components/Overlays/Info/BiodiversityCard.tsx
+++ web/src/components/Overlays/Info/BiodiversityCard.tsx
@@ -54,10 +54,78 @@ export const BiodiversityCard = ({ activeProjectData }) => {
               dataDeficientPercentage,
             }
           })
+          const treePlantings = `${project.name
+            .toLowerCase()
+            //removes whitespace and underscores
+            .split(/[\s_]+/)
+            .join('-')}-all-tree-plantings.geojson`
+          fetch(`${process.env.AWS_STORAGE}/shapefiles/${treePlantings}`)
+            .then((response) => response.json())
+            .then((json) => {
+              console.log(json)
+              const allSpecies = new Set()
+              const similarityThreshold = 3
+              json.features.map((tree) => {
+                let species = tree.properties.species
+                if (species) {
+                  species = species.trim()
+                  species = toTitleCase(species)
+                  let isSimilar = false
+                  allSpecies.forEach((existingSpecies) => {
+                    const distance = levenshteinDistance(
+                      species,
+                      existingSpecies
+                    )
+                    if (distance <= similarityThreshold) {
+                      isSimilar = true
+                    }
+                  })
+
+                  if (!isSimilar) {
+                    allSpecies.add(species)
+                  }
+                }
+              })
+              console.log(allSpecies)
+            })
           return setBiodiversity(biodiversity)
         })
     }
   }, [activeProjectData])
+
+  // checks for typos between instances
+  function levenshteinDistance(a, b) {
+    const matrix = []
+
+    for (let i = 0; i <= b.length; i++) {
+      matrix[i] = [i]
+    }
+
+    for (let j = 0; j <= a.length; j++) {
+      matrix[0][j] = j
+    }
+
+    for (let i = 1; i <= b.length; i++) {
+      for (let j = 1; j <= a.length; j++) {
+        if (b.charAt(i - 1) === a.charAt(j - 1)) {
+          matrix[i][j] = matrix[i - 1][j - 1]
+        } else {
+          matrix[i][j] = Math.min(
+            matrix[i - 1][j - 1] + 1,
+            Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1)
+          )
+        }
+      }
+    }
+
+    return matrix[b.length][a.length]
+  }
+
+  const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    })
+  }
 
   return (
     <InfoBox>

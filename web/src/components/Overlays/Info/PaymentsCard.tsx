b15535a34a9e2c52d20e7798a4c8364233f32f32
Zach Lefler <zachary.lefler@gmail.com>
2024-01-03 11:56:22 -0800

    add exchange rate, filter duplicate transactions

--- web/src/components/Overlays/Info/PaymentsCard.tsx
+++ web/src/components/Overlays/Info/PaymentsCard.tsx
@@ -4,117 +4,46 @@ import ThemedSkeleton from '../../Map/components/Skeleton'
 
 import { InfoBox } from './InfoBox'
 export const PaymentCard = ({ activeProjectData }) => {
-  const dummyData = {
-    message: 'OK',
-    result: [
-      {
-        value: '969798115140192',
-        blockHash:
-          '0x94079e6671606b8dd93c427cb1ada54bec5edd19d668d75e98de769a2aa8feea',
-        blockNumber: '21252221',
-        confirmations: '2074104',
-        contractAddress: '0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73',
-        cumulativeGasUsed: '638663',
-        from: '0xbf8480fc387b72892ca28f4e9f07f95ed5672b3f',
-        gas: '162637',
-        gasPrice: '10039816835',
-        gasUsed: '120744',
-        hash: '0xf31043bc9b73b2107c91f1c603142a824a0d43f5d1151cc90b1ebef8e2d5bfc4',
-        input:
-          '0xac16dbd8000000000000000000000000bf8480fc387b72892ca28f4e9f07f95ed5672b3f00000000000000000000000000000000000000000000000000000000000000e0000000000000000000000000d8763cba276a3738e6de85b4b3bf5fded6d6ca73000000000000000000000000000000000000000000000000009da39ef9af8665000000000000000000000000000000000000000000000000000000000000001bf3c22b4d4ecf9927b5d5d0f3e32bfcf49be85e587e9e62676f7c5f1954d4905c78b6af01aa0da8666d0306b7ff111514705113564b38eee6e4eb3942ea37bdc700000000000000000000000000000000000000000000000000000000000000503078626239653238323038636364313130636462363637343137653361653062326562653338653831616132623432643730636238323935623765626137313535612d3230373339352d32303836303500000000000000000000000000000000',
-        logIndex: '20',
-        nonce: '83',
-        timeStamp: '1693929591',
-        to: '0x6af4da1576bbeaf1a7a2ae695c508dab93510ff4',
-        tokenDecimal: '18',
-        tokenName: 'Celo Euro',
-        tokenSymbol: 'cEUR',
-        transactionIndex: '5',
-      },
-      {
-        value: '242449528785048',
-        blockHash:
-          '0x94079e6671606b8dd93c427cb1ada54bec5edd19d668d75e98de769a2aa8feea',
-        blockNumber: '21252221',
-        confirmations: '2074104',
-        contractAddress: '0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73',
-        cumulativeGasUsed: '638663',
-        from: '0xbf8480fc387b72892ca28f4e9f07f95ed5672b3f',
-        gas: '162637',
-        gasPrice: '10039816835',
-        gasUsed: '120744',
-        hash: '0xf31043bc9b73b2107c91f1c603142a824a0d43f5d1151cc90b1ebef8e2d5bfc4',
-        input:
-          '0xac16dbd8000000000000000000000000bf8480fc387b72892ca28f4e9f07f95ed5672b3f00000000000000000000000000000000000000000000000000000000000000e0000000000000000000000000d8763cba276a3738e6de85b4b3bf5fded6d6ca73000000000000000000000000000000000000000000000000009da39ef9af8665000000000000000000000000000000000000000000000000000000000000001bf3c22b4d4ecf9927b5d5d0f3e32bfcf49be85e587e9e62676f7c5f1954d4905c78b6af01aa0da8666d0306b7ff111514705113564b38eee6e4eb3942ea37bdc700000000000000000000000000000000000000000000000000000000000000503078626239653238323038636364313130636462363637343137653361653062326562653338653831616132623432643730636238323935623765626137313535612d3230373339352d32303836303500000000000000000000000000000000',
-        logIndex: '19',
-        nonce: '83',
-        timeStamp: '1693929591',
-        to: '0xd533ca259b330c7a88f74e000a3faea2d63b7972',
-        tokenDecimal: '18',
-        tokenName: 'Celo Euro',
-        tokenSymbol: 'cEUR',
-        transactionIndex: '5',
-      },
-      {
-        value: '44371474532763237',
-        blockHash:
-          '0x94079e6671606b8dd93c427cb1ada54bec5edd19d668d75e98de769a2aa8feea',
-        blockNumber: '21252221',
-        confirmations: '2074104',
-        contractAddress: '0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73',
-        cumulativeGasUsed: '638663',
-        from: '0x530cf57b70c22c0437bd77adebd79fe58c42dc2a',
-        gas: '162637',
-        gasPrice: '10039816835',
-        gasUsed: '120744',
-        hash: '0xf31043bc9b73b2107c91f1c603142a824a0d43f5d1151cc90b1ebef8e2d5bfc4',
-        input:
-          '0xac16dbd8000000000000000000000000bf8480fc387b72892ca28f4e9f07f95ed5672b3f00000000000000000000000000000000000000000000000000000000000000e0000000000000000000000000d8763cba276a3738e6de85b4b3bf5fded6d6ca73000000000000000000000000000000000000000000000000009da39ef9af8665000000000000000000000000000000000000000000000000000000000000001bf3c22b4d4ecf9927b5d5d0f3e32bfcf49be85e587e9e62676f7c5f1954d4905c78b6af01aa0da8666d0306b7ff111514705113564b38eee6e4eb3942ea37bdc700000000000000000000000000000000000000000000000000000000000000503078626239653238323038636364313130636462363637343137653361653062326562653338653831616132623432643730636238323935623765626137313535612d3230373339352d32303836303500000000000000000000000000000000',
-        logIndex: '17',
-        nonce: '83',
-        timeStamp: '1693929591',
-        to: '0xbf8480fc387b72892ca28f4e9f07f95ed5672b3f',
-        tokenDecimal: '18',
-        tokenName: 'Celo Euro',
-        tokenSymbol: 'cEUR',
-        transactionIndex: '5',
-      },
-    ],
-  }
-
   const [paymentData, setPaymentData] = useState([])
-  const gainforestWallet = '0xBF8480fc387b72892Ca28F4e9F07F95ed5672b3f'
+  const [exchangeRate, setExchangeRate] = useState(1)
+  const gainforestWallet = '0xbf8480fc387b72892ca28f4e9f07f95ed5672b3f'
 
   useEffect(() => {
     const addresses = activeProjectData?.project?.CommunityMember.map(
       (member) => member?.Wallet?.CeloAccount
     )
+
     if (addresses) {
       fetch(
         `https://explorer.celo.org/mainnet/api?module=account&action=tokentx&address=${gainforestWallet}`
       )
         .then((res) => res.json())
         .then((data) => {
-          const transactions = data['result']
-          // .filter((transaction) =>
-          //   addresses.includes(transaction.to)
-          // )
+          const seen = new Set()
+          const transactions = data['result'].filter((transaction) => {
+            // current fetch is returning duplicate transactions
+            const isNew = !seen.has(transaction.hash)
+            seen.add(transaction.hash)
+            return (
+              isNew &&
+              transaction['from'].toString() == gainforestWallet &&
+              addresses.includes(transaction.to)
+            )
+          })
           if (transactions.length > 0) {
             setPaymentData(transactions)
           }
-          // else {
-          //   setPaymentData(dummyData.result)
-          // }
         })
+      fetch(
+        'https://api.coingecko.com/api/v3/simple/price?ids=celo-euro&vs_currencies=usd'
+      )
+        .then((res) => res.json())
+        .then((data) => setExchangeRate(data['celo-euro'].usd))
     }
-  })
+  }, [])
 
-  const getBlockDate = (blockNumber) => {
-    const genesisDate = new Date('2020-04-22T16:00:00.000Z')
-    const blockTime = 5
-    const blockDate = new Date(
-      genesisDate.getTime() + blockNumber * blockTime * 1000
-    )
+  const getBlockDate = (timeStamp) => {
+    const blockDate = new Date(timeStamp * 1000)
     return blockDate.toLocaleDateString('en-US', {
       day: '2-digit',
       month: 'long',
@@ -152,10 +81,10 @@ export const PaymentCard = ({ activeProjectData }) => {
         <div>
           {paymentData.map((payment) => {
             return (
-              <div style={{ marginTop: '32px' }} key={payment.blockHash}>
+              <div style={{ marginTop: '32px' }} key={payment.hash}>
                 <div style={{ display: 'flex' }}>
                   <div style={{ marginLeft: '16px' }}>
-                    <h3> {getBlockDate(payment.blockNumber)}</h3>
+                    <h3> {getBlockDate(payment.timeStamp)}</h3>
                     <p>
                       To:{' '}
                       <a
@@ -174,7 +103,7 @@ export const PaymentCard = ({ activeProjectData }) => {
                       </a>
                     </p>
                     <p style={{ color: '#67962A' }}>
-                      ${(payment.value / 1e18).toFixed(6)}
+                      ${((payment.value / 1e18) * exchangeRate).toFixed(6)}
                     </p>
                   </div>
                 </div>

92ac10800f2c457bd2f09bc0b1867c9150c9168c
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-05-22 14:51:07 +0200

    Separate out nasa source and layers

--- web/src/components/Map/maputils.ts
+++ web/src/components/Map/maputils.ts
@@ -40,13 +40,10 @@ export const addAllSourcesAndLayers = (
   addTreeCoverSourceAndLayer(map)
   addAllSitesSourceAndLayer(map)
   addHighlightedSiteSourceAndLayer(map)
-  // addNasaSourceAndLayer(map)
   // addHexagonsSourceAndLayers(map, hexagons)
   addHiveSourceAndLayers(map, hiveLocations, setMarkers)
   addTreesPlantedSourceAndLayers(map)
 }
-
-// https://gibs-c.earthdata.nasa.gov/wmts/epsg4326/best/wmts.cgi?TIME=2023-07-15T00:00:00Z&layer=VIIRS_NOAA20_CorrectedReflectance_TrueColor&style=default&tilematrixset=250m&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix=1&TileCol=1&TileRow=0
 
 export const addHiveSourceAndLayers = (
   map: mapboxgl.Map,
@@ -64,30 +61,6 @@ export const toggleOrthomosaic = (map: mapboxgl.Map, visibility) => {
     map.setLayoutProperty('orthomosaic', 'visibility', visibility)
   }
 }
-
-// export const addNasaSourceAndLayer = (map: mapboxgl.Map) => {
-//   // const tilePath =
-//   //   'wmts/epsg4326/best/' +
-//   //   'MODIS_Terra_CorrectedReflectance_TrueColor/default/' +
-//   //   '2018-06-01/GoogleMapsCompatible_Level9/{z}/{y}/{x}.jpg'
-
-//   const tilePath =
-//     'https://gibs-c.earthdata.nasa.gov/wmts/epsg4326/best/wmts.cgi?TIME=2023-07-15T00:00:00Z&layer=VIIRS_NOAA20_CorrectedReflectance_TrueColor&style=default&tilematrixset=250m&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix=1&TileCol=1&TileRow=0'
-
-//   console.log('im adding nasa ')
-//   map.addSource('nasaSource', {
-//     type: 'raster',
-//     tiles: [
-//       'https://gitc-b.earthdata.nasa.gov/wmts/epsg4326/best/wmts.cgi?TIME=2023-07-16T00:00:00Z&layer=VIIRS_SNPP_CorrectedReflectance_TrueColor&style=default&tilematrixset=250m&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}',
-//     ],
-//     tileSize: 256,
-//   })
-//   map.addLayer({
-//     id: 'nasaLayer',
-//     type: 'raster',
-//     source: 'nasaSource',
-//   })
-// }
 
 export const addClickableMarkers = (
   map: mapboxgl.Map,

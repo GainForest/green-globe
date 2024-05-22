ac7f6016c543046c048b3a7a8cdfdb6dabe05131
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-05-22 14:31:24 +0200

    Add greyscale source and layers

---
+++ web/src/components/Map/sourcesAndLayers/greyscaleTerrain.ts
@@ -0,0 +1,43 @@
+import mapboxgl from 'mapbox-gl'
+
+export const addGreyscaleSourceAndLayers = (map: mapboxgl.Map) => {
+  map.addLayer({
+    id: 'greyscale',
+    type: 'raster',
+    source: {
+      type: 'raster',
+      url: 'mapbox://mapbox.satellite',
+      tileSize: 256,
+    },
+    paint: {
+      'raster-saturation': -1, // Apply greyscale by desaturating the colors
+    },
+  })
+  map.addLayer({
+    id: 'white-overlay',
+    type: 'fill',
+    source: {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [-180, -85],
+              [-180, 85],
+              [180, 85],
+              [180, -85],
+              [-180, -85],
+            ],
+          ],
+        },
+      },
+    },
+    layout: {},
+    paint: {
+      'fill-color': '#ffffff',
+      'fill-opacity': 0, // Adjust the opacity as needed
+    },
+  })
+}

e7d97cd6622b36abd3d8cf48e9062d6904e21580
Zach Lefler <zachary.lefler@gmail.com>
2024-05-23 13:31:03 -0700

    refactor blog.tsx, remove methodology classification from header

--- web/src/pages/InsightsPage/InsightsPage.tsx
+++ web/src/pages/InsightsPage/InsightsPage.tsx
@@ -1,18 +1,12 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 
 import DOMpurify from 'dompurify'
-import styled from 'styled-components'
 
-import BlogPost from 'src/components/Blog/BlogPost'
+import Blog from 'src/components/Blog/Blog'
 
-const Blog = () => {
+const InsightsPage = () => {
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(true)
-  const [opacity, setOpacity] = useState(0)
-  const [displayedPosts, setDisplayedPosts] = useState([])
-  const [postIndex, setPostIndex] = useState(0)
-  const contentRef = useRef(null)
-  const postRefs = useRef([])
 
   const formatDate = (timestamp) => {
     const date = new Date(timestamp)
@@ -21,10 +15,6 @@ const Blog = () => {
     const day = date.getDate()
     return `${day} ${month}, ${year}`
   }
-
-  useEffect(() => {
-    postRefs.current = postRefs.current.slice(0, displayedPosts.length)
-  }, [displayedPosts])
 
   useEffect(() => {
     const getPosts = async () => {
@@ -47,223 +37,13 @@ const Blog = () => {
         (d) => !d.categories.includes('Methodology')
       )
       setPosts(uncategorizedPosts)
-      if (uncategorizedPosts.length > 0) {
-        setDisplayedPosts([uncategorizedPosts[0]])
-      }
 
       setLoading(false)
     }
     getPosts()
   }, [])
 
-  useEffect(() => {
-    const container = contentRef.current
-
-    const handleScroll = () => {
-      const closestPostIndex = postRefs.current.reduce(
-        (closestIndex, el, index) => {
-          const box = el.getBoundingClientRect()
-          const prevBox =
-            postRefs.current[closestIndex]?.getBoundingClientRect()
-          if (!prevBox) return index
-          if (Math.abs(box.top) < Math.abs(prevBox.top)) return index
-          return closestIndex
-        },
-        0
-      )
-
-      if (closestPostIndex !== postIndex) {
-        setPostIndex(closestPostIndex)
-      }
-
-      // Check if near the bottom of the container to load more posts
-      if (
-        container.scrollHeight - container.scrollTop <=
-        container.clientHeight * 1.5
-      ) {
-        const currentLength = displayedPosts.length
-        const morePosts = posts.slice(currentLength, currentLength + 5) // Load 5 more posts, adjust as needed
-        if (morePosts.length > 0) {
-          setDisplayedPosts([...displayedPosts, ...morePosts])
-        }
-      }
-    }
-
-    container.addEventListener('scroll', handleScroll)
-
-    return () => {
-      container.removeEventListener('scroll', handleScroll)
-    }
-  }, [displayedPosts, posts, postIndex])
-
-  useEffect(() => {
-    postRefs.current = postRefs.current.slice(0, displayedPosts.length)
-  }, [displayedPosts])
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setOpacity(1)
-    }, 300)
-    return () => clearTimeout(timer)
-  }, [])
-
-  const handlePostClick = (post, index) => {
-    setOpacity(0)
-    setTimeout(() => {
-      setOpacity(1)
-    }, 300)
-    if (index > displayedPosts.length - 1) {
-      const newPosts = posts.slice(displayedPosts.length, index + 1)
-      setDisplayedPosts([...displayedPosts, ...newPosts])
-    }
-    setPostIndex(index)
-    setTimeout(() => {
-      postRefs.current[index]?.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      })
-    }, 0)
-  }
-
-  return (
-    <Container loading={loading}>
-      <PostList>
-        {posts.map((post, index) => (
-          <PostPreview
-            key={index}
-            selected={index == postIndex}
-            onClick={() => handlePostClick(post, index)}
-          >
-            <p style={{ fontWeight: 'lighter', fontSize: '12px' }}>
-              {post.categories}
-            </p>
-            <img src={post.thumbnail} alt={post.title} />
-            <PreviewTitle>{post.title}</PreviewTitle>
-          </PostPreview>
-        ))}
-      </PostList>
-      <MainContent>
-        <LoadingMessage loading={loading}>Loading...</LoadingMessage>
-        <Content loading={loading} opacity={opacity} ref={contentRef}>
-          {displayedPosts?.map((post, index) => (
-            <PostContainer
-              key={post.title}
-              ref={(el) => (postRefs.current[index] = el)}
-            >
-              <PostHeader>
-                <p style={{ fontWeight: 'lighter' }}>
-                  {post.categories.join(', ')}
-                </p>
-                <PostDate>{post.date}</PostDate>
-              </PostHeader>
-              <PostTitle>{post.title}</PostTitle>
-              <BlogPost content={post.content} />
-            </PostContainer>
-          ))}
-        </Content>
-      </MainContent>
-    </Container>
-  )
+  return <Blog posts={posts} loading={loading} />
 }
 
-export default Blog
-
-const Container = styled.div`
-  display: flex;
-  overflow: hidden;
-  transition: opacity 1s ease;
-  max-height: calc(100vh - 52px);
-`
-
-const PostList = styled.div`
-  width: 180px;
-  background: transparent;
-  overflow-y: auto;
-  border-right: 1px solid #ddd;
-`
-
-const PostPreview = styled.div`
-  padding: 8px 16px;
-  cursor: pointer;
-  display: block;
-  align-items: center;
-  background: ${({ selected }) => (selected ? '#999' : '#333')};
-  border-radius: 4px;
-  margin: 16px;
-  img {
-    width: 114px;
-    height: 57px;
-    margin-right: 16px;
-    object-fit: cover;
-  }
-  &:hover {
-    background: #555;
-    transition: background 0.3s ease;
-  }
-`
-
-const PreviewTitle = styled.div`
-  font-size: 16px;
-  color: white;
-`
-
-const MainContent = styled.div`
-  flex: 1;
-  overflow-y: auto;
-  max-height: calc(100vh - 52px);
-  background-image: url('/blog-bg.jpeg');
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center;
-`
-
-const LoadingMessage = styled.div`
-  opacity: ${(props) => (props.loading ? 1 : 0)};
-  transition: opacity 1s ease;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 24px;
-  color: #333;
-`
-
-const Content = styled.div`
-  max-height: calc(100vh - 52px);
-  overflow-y: auto;
-  padding: 0 16px;
-  opacity: ${(props) => props.opacity};
-  transition: opacity 0.3s ease;
-`
-
-const PostContainer = styled.div`
-  margin: 40px 0 80px 0;
-  background-color: rgba(85, 85, 85, 0.5);
-  padding: 8px;
-  max-width: 620px;
-  border-radius: 4px;
-`
-
-const PostHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-
-const PostTitle = styled.h1`
-  float: left
-  display: inline;
-`
-
-const PostDate = styled.p`
-  display: inline;
-  float: right;
-`
-
-const PostContent = styled.div`
-  img {
-    max-width: 620px;
-    width: 100%;
-    object-fit: contain;
-  }
-`
+export default InsightsPage

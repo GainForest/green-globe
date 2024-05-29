7563f287f6eb32a84d8ba62fdc4eaaf944abc2da
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-05-29 17:12:07 +0200

    Fix drone map not displaying

--- web/src/pages/SurveyOverviewPage/Map/DroneFlightsMap.tsx
+++ web/src/pages/SurveyOverviewPage/Map/DroneFlightsMap.tsx
@@ -3,10 +3,17 @@ import { useEffect, useState } from 'react'
 import { initializeMapbox } from 'src/mapbox.config'
 
 export const DroneFlightsMap = () => {
-  const [map, setMap] = useState()
+  const [map, setMap] = useState<mapboxgl.Map>()
 
   useEffect(() => {
-    initializeMapbox('drone-flights-map-container', setMap)
+    initializeMapbox(
+      'drone-flights-map-container',
+      setMap,
+      [
+        -54.47573384080506, -0.0019358806309526244, -64.16146283459103,
+        -6.463821341171396,
+      ]
+    )
   }, [])
 
   return (

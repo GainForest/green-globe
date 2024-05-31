37f8738861818ee45a486118ea6caa41ec7188ea
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-05-31 03:23:29 +0200

    Map through drone datasets

--- web/src/components/Map/sourcesAndLayers/cogSourceAndLayers.ts
+++ web/src/components/Map/sourcesAndLayers/cogSourceAndLayers.ts
@@ -1,23 +1,37 @@
 import mapboxgl from 'mapbox-gl'
 
+const TITILER_ENDPOINT =
+  'http://127.0.0.1:8000/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@1x?url=file://'
+
+const ALLTILES = [
+  '/Users/sharfy/Code/xprize-2/web/public/data/drone/1_webmercator.tif',
+  '/Users/sharfy/Code/xprize-2/web/public/data/drone/20240414_134447_ssc15_u0001_visual_clip_webmercator.tif',
+  '/Users/sharfy/Code/xprize-2/web/public/data/drone/20240414_134447_ssc15_u0002_visual_clip_webmercator.tif',
+  '/Users/sharfy/Code/xprize-2/web/public/data/drone/20240428_191813_ssc16_u0001_visual_clip_webmercator.tif',
+  '/Users/sharfy/Code/xprize-2/web/public/data/drone/20240428_191813_ssc16_u0002_visual_clip_webmercator.tif',
+  '/Users/sharfy/Documents/drone/ingles/24_webmercator_cog.tif',
+  '/Users/sharfy/Documents/drone/ingles/23_webmercator_cog.tif',
+  '/Users/sharfy/Documents/drone/ingles/dsm_webmercator.tif',
+]
+
 export const addCOGSourceAndLayers = (map: mapboxgl.Map) => {
-  if (!map.getSource('COGSource')) {
-    map.addSource('COGSource', {
-      type: 'raster',
-      tiles: [
-        'http://127.0.0.1:8000/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@1x?url=file:///Users/sharfy/Downloads/mosaic/1_webmercator.tif',
-      ],
-      tileSize: 256,
-    })
-  }
-  if (!map.getLayer('COGLayer')) {
-    map.addLayer({
-      id: 'COGLayer',
-      type: 'raster',
-      source: 'COGSource',
-      paint: {
-        'raster-opacity': 1,
-      },
-    })
-  }
+  ALLTILES.forEach((d) => {
+    if (!map.getSource(`${d}Source`)) {
+      map.addSource(`${d}Source`, {
+        type: 'raster',
+        tiles: [`${TITILER_ENDPOINT}${d}`],
+        tileSize: 256,
+      })
+    }
+    if (!map.getLayer(`${d}Layer`)) {
+      map.addLayer({
+        id: `${d}Layer`,
+        type: 'raster',
+        source: `${d}Source`,
+        paint: {
+          'raster-opacity': 1,
+        },
+      })
+    }
+  })
 }

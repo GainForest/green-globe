2086642c2af8de12628024c2df3203b47608b5d8
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-06-09 11:15:05 +0200

    Separate out MeasuredDataGrid

--- web/src/components/Overlays/Info/BiodiversityCard/BiodiversityCard.tsx
+++ web/src/components/Overlays/Info/BiodiversityCard/BiodiversityCard.tsx
@@ -13,7 +13,7 @@ import {
   fetchTreePlantings,
   processBiodiversityData,
 } from './biodiversityCardHelpers'
-import { MeasuredDataPhoto } from './MeasuredDataPhoto'
+
 export const BiodiversityCard = ({
   activeProjectData,
   mediaSize,
@@ -172,82 +172,5 @@ const PredictedAnimalsGrid = ({ biodiversity }) => {
         <div></div>
       </>
     )
-  }
-}
-
-const MeasuredDataGrid = ({
-  sortBy,
-  setSortBy,
-  measuredData,
-  handleSpeciesClick,
-  selectedSpecies,
-  loading,
-  mediaSize,
-}) => {
-  if (loading) {
-    return (
-      <>
-        <h3>
-          <ThemedSkeleton width={'120px'} />
-        </h3>
-        <div></div>
-      </>
-    )
-  } else {
-    if (measuredData.length > 0) {
-      return (
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          {measuredData.map((group, idx) => (
-            <div style={{ flex: '1 1 50%' }} key={group.title + idx}>
-              <div
-                style={{
-                  border: '1px solid #2f3030',
-                  height: '80px',
-                  width: '160px',
-                  margin: '16px 0px',
-                  borderRadius: '10px',
-                }}
-              >
-                <h4 style={{ margin: '16px' }}>
-                  Total {group.title.toLowerCase()}
-                </h4>
-                <h2 style={{ float: 'right', margin: '0 16px' }}>
-                  {group.total}
-                </h2>
-              </div>
-              <h3>{group.title}</h3>
-              {group.title === 'Trees' && (
-                <p style={{ marginTop: '8px' }}>
-                  Click a species to highlight them on the map
-                </p>
-              )}
-              <p>Sort By:</p>
-              <ToggleButton
-                mediaSize={mediaSize}
-                active={sortBy}
-                setToggle={setSortBy}
-                options={['Name', 'Count']}
-              />
-              {group.species.map((species) => (
-                <div
-                  className={
-                    species.name == selectedSpecies ? null : 'species-button'
-                  }
-                  key={species.name}
-                >
-                  <MeasuredDataPhoto
-                    {...species}
-                    handleSpeciesClick={handleSpeciesClick}
-                    selectedSpecies={selectedSpecies}
-                  />
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      )
-    } else {
-      return <p>There is not yet any measured biodiversity for this project.</p>
-    }
   }
 }

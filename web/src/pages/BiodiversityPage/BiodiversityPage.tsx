c626248056b9a2ef3e92ac1423db2253aa06b777
Zach Lefler <zachary.lefler@gmail.com>
2024-06-10 12:19:50 -0700

    format table

--- web/src/pages/BiodiversityPage/BiodiversityPage.tsx
+++ web/src/pages/BiodiversityPage/BiodiversityPage.tsx
@@ -1,107 +1,129 @@
 import { useEffect, useState } from 'react'
 
-import DataTable from 'react-data-table-component'
-
 import { MetaTags } from '@redwoodjs/web'
-
-const columns = [
-  {
-    name: 'Time',
-    selector: (row) => row.eventDate,
-  },
-  {
-    name: 'Scientific Name',
-    selector: (row) => row.scientificName,
-    sortable: true,
-  },
-  {
-    name: 'Basis of record',
-    selector: (row) => row.basisOfRecord,
-    sortable: true,
-  },
-]
 
 const BiodiversityPage = () => {
   const [observations, setObservations] = useState([])
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     await fetch(`${process.env.AWS_STORAGE}/observations/semifinals.json`)
-  //       .then((response) => response.json())
-  //       .then((res) => setObservations(res))
-  //   }
-  //   fetchData()
-  // }, [])
-
-  // useEffect(() => {
-  //   console.log(observations)
-  // }, [observations])
+  const [photoMap, setPhotoMap] = useState({})
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(
-        `${process.env.AWS_STORAGE}/observations/semifinals.json`
-      )
-      const data = await res.json()
-
-      data.slice(0, 20).forEach(async (row) => {
-        const observation = row
-        const name = row.scientificName.split(' ').join('%2B')
-        const eolRes = await fetch(
-          `https://eol.org/api/search/1.0.json?q=${name}&page=1&key=`
-        )
-        const eolData = await eolRes.json()
-        const eolId = eolData.results[0]?.id
-        if (eolId) {
-          console.log(eolId)
-          const eolSpeciesRes = await fetch(
-            `https://eol.org/api/pages/1.0/${eolId}.json?details=true&images_per_page=1`
-          )
-          const eolSpeciesData = await eolSpeciesRes.json()
-          console.log(eolSpeciesData?.taxonConcept?.dataObjects[0])
-          const mediaLink =
-            eolSpeciesData?.taxonConcept?.dataObjects[0]?.mediaURL
-          if (mediaLink) {
-            observation.image_url = mediaLink
-          }
-        } else {
-          console.log('no id')
-        }
-        setObservations((observations) => [...observations, observation])
-      })
+      await fetch(`${process.env.AWS_STORAGE}/observations/semifinals.json`)
+        .then((response) => response.json())
+        .then((res) => setObservations(res))
     }
     fetchData()
   }, [])
 
+  // useEffect(() => {
+  //   const fetchPhotos = async () => {
+  //     const fetchPhoto = async (row) => {
+  //       const name = encodeURIComponent(row.scientificName)
+  //       console.log(name)
+  //       const eolRes = await fetch(
+  //         `https://eol.org/api/search/1.0.json?q=${name}&page=1&key=`
+  //       )
+  //       const eolData = await eolRes.json()
+  //       const eolId = eolData.results[0]?.id
+  //       if (eolId) {
+  //         const eolSpeciesRes = await fetch(
+  //           `https://eol.org/api/pages/1.0/${eolId}.json?details=true&images_per_page=1`
+  //         )
+  //         const eolSpeciesData = await eolSpeciesRes.json()
+  //         return eolSpeciesData?.taxonConcept?.dataObjects[0]?.mediaURL || ''
+  //       }
+  //       return ''
+  //     }
+
+  //     const batchSize = 10
+  //     for (let i = 0; i < observations.length; i += batchSize) {
+  //       const batch = observations.slice(i, i + batchSize)
+  //       const photos = await Promise.all(batch.map((row) => fetchPhoto(row)))
+  //       const newPhotoMap = photos.reduce((map, photo, index) => {
+  //         if (photo) {
+  //           map[batch[index].scientificName] = photo
+  //         }
+  //         return map
+  //       }, {})
+  //       setPhotoMap((prevMap) => ({ ...prevMap, ...newPhotoMap }))
+  //     }
+  //   }
+
+  //   if (observations.length) {
+  //     fetchPhotos()
+  //   }
+  // }, [observations])
+
+  const camelCaseToTitleCase = (str) => {
+    return str
+      .replace(/([A-Z])/g, ' $1')
+      .trim()
+      .toLowerCase()
+      .replace(/\b(\w)/g, (match) => match.toUpperCase())
+  }
+
+  const formatDate = (date) => {
+    return new Date(date).toLocaleString('en-GB')
+  }
+
   return (
     <>
       <MetaTags title="Biodiversity" description="Biodiversity page" />
-
       <h1>Observations</h1>
-      {observations.length &&
-        observations.map((row, idx) => (
-          <div className="species-button" key={row.scientificName + idx}>
-            <img
-              alt={row.scientificName}
-              src={row.image_url}
+      <div style={{ overflowX: 'auto' }}>
+        <table
+          style={{
+            width: '100%',
+            borderCollapse: 'collapse',
+            border: '1px solid #ccc',
+          }}
+        >
+          <thead>
+            <tr
               style={{
-                objectFit: 'cover',
-                clipPath:
-                  'polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%)',
-                height: '120px',
-                width: '120px',
-                minWidth: '120px',
+                backgroundColor: '#333333',
+                color: '#ffffff',
+                fontWeight: 'bold',
+                textAlign: 'left',
               }}
-            />
-          </div>
-        ))}
-
-      {/* <DataTable
-        columns={columns}
-        data={observations}
-        highlightOnHover
-        pagination
-      /> */}
+            >
+              <th style={{ padding: '8px' }}>Photo</th>
+              <th style={{ padding: '8px' }}>Name</th>
+              <th style={{ padding: '8px', textAlign: 'center' }}>Time</th>
+              <th style={{ padding: '8px' }}>Basis of Record</th>
+            </tr>
+          </thead>
+          <tbody>
+            {observations.map((row, idx) => (
+              <tr
+                key={idx}
+                style={{
+                  backgroundColor: idx % 2 ? '#404040' : '#333333',
+                  color: '#ffffff',
+                  height: '200px',
+                }}
+              >
+                <td style={{ padding: '8px', textAlign: 'center' }}>
+                  {photoMap[row.scientificName] && (
+                    <img
+                      src={photoMap[row.scientificName]}
+                      alt={row.scientificName}
+                      style={{ width: '100px', height: '100px' }}
+                    />
+                  )}
+                </td>
+                <td style={{ padding: '8px' }}>{row.scientificName}</td>
+                <td style={{ padding: '8px', textAlign: 'center' }}>
+                  {formatDate(row.eventDate)}
+                </td>
+                <td style={{ padding: '8px' }}>
+                  {camelCaseToTitleCase(row.basisOfRecord)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }

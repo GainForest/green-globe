21f5fa390097f25bf763ecd1c3bf68f8e5794875
Zach Lefler <zachary.lefler@gmail.com>
2024-06-10 11:11:40 -0700

    add EOL fetch

--- web/src/pages/BiodiversityPage/BiodiversityPage.tsx
+++ web/src/pages/BiodiversityPage/BiodiversityPage.tsx
@@ -24,11 +24,51 @@ const columns = [
 const BiodiversityPage = () => {
   const [observations, setObservations] = useState([])
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     await fetch(`${process.env.AWS_STORAGE}/observations/semifinals.json`)
+  //       .then((response) => response.json())
+  //       .then((res) => setObservations(res))
+  //   }
+  //   fetchData()
+  // }, [])
+
+  // useEffect(() => {
+  //   console.log(observations)
+  // }, [observations])
+
   useEffect(() => {
     const fetchData = async () => {
-      await fetch(`${process.env.AWS_STORAGE}/observations/semifinals.json`)
-        .then((response) => response.json())
-        .then((res) => setObservations(res))
+      const res = await fetch(
+        `${process.env.AWS_STORAGE}/observations/semifinals.json`
+      )
+      const data = await res.json()
+
+      data.slice(0, 20).forEach(async (row) => {
+        const observation = row
+        const name = row.scientificName.split(' ').join('%2B')
+        const eolRes = await fetch(
+          `https://eol.org/api/search/1.0.json?q=${name}&page=1&key=`
+        )
+        const eolData = await eolRes.json()
+        const eolId = eolData.results[0]?.id
+        if (eolId) {
+          console.log(eolId)
+          const eolSpeciesRes = await fetch(
+            `https://eol.org/api/pages/1.0/${eolId}.json?details=true&images_per_page=1`
+          )
+          const eolSpeciesData = await eolSpeciesRes.json()
+          console.log(eolSpeciesData?.taxonConcept?.dataObjects[0])
+          const mediaLink =
+            eolSpeciesData?.taxonConcept?.dataObjects[0]?.mediaURL
+          if (mediaLink) {
+            observation.image_url = mediaLink
+          }
+        } else {
+          console.log('no id')
+        }
+        setObservations((observations) => [...observations, observation])
+      })
     }
     fetchData()
   }, [])
@@ -38,12 +78,30 @@ const BiodiversityPage = () => {
       <MetaTags title="Biodiversity" description="Biodiversity page" />
 
       <h1>Observations</h1>
-      <DataTable
+      {observations.length &&
+        observations.map((row, idx) => (
+          <div className="species-button" key={row.scientificName + idx}>
+            <img
+              alt={row.scientificName}
+              src={row.image_url}
+              style={{
+                objectFit: 'cover',
+                clipPath:
+                  'polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%)',
+                height: '120px',
+                width: '120px',
+                minWidth: '120px',
+              }}
+            />
+          </div>
+        ))}
+
+      {/* <DataTable
         columns={columns}
         data={observations}
         highlightOnHover
         pagination
-      />
+      /> */}
     </>
   )
 }

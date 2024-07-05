c67e5cf3d0b4ce455c4f778511043967238d1614
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-07-05 16:08:32 +0700

    Remove the zoom controls

--- web/src/mapbox.config.ts
+++ web/src/mapbox.config.ts
@@ -39,6 +39,7 @@ export const initializeMapbox = (
         zoom: 2,
         center: [102, 9],
         bounds: XPRIZE_FINALS_BOUNDS_ZOOMED_OUT,
+        zoomControl: false,
       })
       map.addControl(new mapboxgl.NavigationControl())
       setMap(map)

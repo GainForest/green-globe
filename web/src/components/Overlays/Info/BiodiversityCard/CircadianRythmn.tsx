3cca6e3883d1cf4c9f3b30bba4a6f8572b40bc96
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-07-07 03:28:40 +0700

    Linting

--- web/src/components/Overlays/Info/BiodiversityCard/CircadianRythmn.tsx
+++ web/src/components/Overlays/Info/BiodiversityCard/CircadianRythmn.tsx
@@ -1,14 +1,13 @@
+import React, { useMemo } from 'react'
+
 import { SOUNDSCAPE_PATHS } from 'config/soundscape_paths.js'
-import React, { useMemo } from 'react';
 
 import { AudioPlayer } from 'src/components/Buttons/AudioPlayer'
 import CircularBarChart from 'src/components/CircularBarChart'
 import CircularLineChart from 'src/components/CircularLineChart'
 import CircularMultiLineChartWithCSVLoader from 'src/components/CircularMultiLineChart'
 
-
 export const CircadianRythmn = () => {
-
   const sampleMultiData = useMemo(() => {
     return Array.from({ length: 100 }, (_, i) => ({
       time: (i / 100) * 24,
@@ -16,9 +15,9 @@ export const CircadianRythmn = () => {
       freq2: Math.random() * 1000,
       freq3: Math.random() * 5000,
       hi: Math.random() * 10000,
-      freq5: Math.random() * 2000
-    }));
-  }, []);
+      freq5: Math.random() * 2000,
+    }))
+  }, [])
 
   return (
     <>
@@ -26,18 +25,36 @@ export const CircadianRythmn = () => {
       <div>
         <p> Measured soundscape of different frequencies.</p>
       </div>
-        <div>
-          <div style={{ width: '100%', aspectRatio: '1 / 1', maxWidth: '600px', margin: 'auto' }}>
-            <CircularBarChart csvPath={`${process.env.AWS_STORAGE}/pmn/max_results_final.csv`}/>
-          </div>
-          <div style={{ width: '100%', aspectRatio: '1 / 1', maxWidth: '600px', margin: 'auto' }}>
-            <CircularMultiLineChartWithCSVLoader csvPath={`${process.env.AWS_STORAGE}/pmn/median_results_final.csv`}/>
-          </div>
-          <AudioPlayer
-            label="Sample"
-            src={`${process.env.AWS_STORAGE}/audio/013-089-Tufted-Tit-Tyrant.mp3`}
+      <div>
+        <div
+          style={{
+            width: '100%',
+            aspectRatio: '1 / 1',
+            maxWidth: '600px',
+            margin: 'auto',
+          }}
+        >
+          <CircularBarChart
+            csvPath={`${process.env.AWS_STORAGE}/pmn/max_results_final.csv`}
           />
         </div>
+        <div
+          style={{
+            width: '100%',
+            aspectRatio: '1 / 1',
+            maxWidth: '600px',
+            margin: 'auto',
+          }}
+        >
+          <CircularMultiLineChartWithCSVLoader
+            csvPath={`${process.env.AWS_STORAGE}/pmn/median_results_final.csv`}
+          />
+        </div>
+        <AudioPlayer
+          label="Sample"
+          src={`${process.env.AWS_STORAGE}/audio/013-089-Tufted-Tit-Tyrant.mp3`}
+        />
+      </div>
     </>
   )
 }

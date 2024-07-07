f12855959d2d09caaed8d23e9e83630900925405
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-07-07 19:47:10 +0700

    Modify toKebabCase test

--- web/src/utils/toKebabCase.ts
+++ web/src/utils/toKebabCase.ts
@@ -1,6 +1,8 @@
 export const toKebabCase = (str) => {
   return str
+    .normalize('NFD') // Normalize to decomposed form
+    .replace(/[\u0300-\u036f]/g, '') // Remove diacritics
     .toLowerCase()
     .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-+|-+$/g, '');
+    .replace(/^-+|-+$/g, '')
 }

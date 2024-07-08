5535bf371b3f64a8cef46154adcf8678c79d9581
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-07-08 16:16:35 +0700

    Conditionally render the dendogram

--- web/src/components/Dendogram/Dendogram.tsx
+++ web/src/components/Dendogram/Dendogram.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useSelector } from 'react-redux'
+import styled from 'styled-components'
 
 import { toKebabCase } from 'src/utils/toKebabCase'
 
 export const Dendogram = () => {
+  const [imageExists, setImageExists] = useState(false)
   const kebabCasedProjectName = useSelector((state: State) =>
     toKebabCase(state.project.name)
   )
+
+  useEffect(() => {
+    const img = new Image()
+    img.src = `${process.env.AWS_STORAGE}/dendogram/${kebabCasedProjectName}dendogram.svg`
+    img.onload = () => setImageExists(true)
+    img.onerror = () => setImageExists(false)
+  }, [kebabCasedProjectName])
+
+  if (!kebabCasedProjectName) {
+    return <Loading />
+  }
+  if (!imageExists) {
+    return <NoData />
+  }
 
   return (
     <div>
       <h2> Species dendogram</h2>
       <p>All species recorded in this project.</p>
       <img
-        src={`${process.env.AWS_STORAGE}/dendogram/${kebabCasedProjectName}dendogram.svg`}
+        src={`${process.env.AWS_STORAGE}/dendogram/${kebabCasedProjectName}-dendogram.svg`}
         alt={'species dendogram'}
         style={{ margin: '20px 0px', backgroundColor: 'white' }}
       />
@@ -22,4 +38,18 @@ export const Dendogram = () => {
   )
 }
 
+const Loading = () => (
+  <Container> Searching for the project dendogram... </Container>
+)
+
+const NoData = () => (
+  <Container>
+    No species dendogram have been generated for this project.
+  </Container>
+)
+
 export default Dendogram
+
+const Container = styled.div`
+  margin: 16px 8px;
+`

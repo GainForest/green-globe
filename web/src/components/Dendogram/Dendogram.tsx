a39570cdf0700192b77cbda0ae705121f1077dbd
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-07-08 15:58:17 +0700

    Dendogram is now project specific

--- web/src/components/Dendogram/Dendogram.tsx
+++ web/src/components/Dendogram/Dendogram.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import { useSelector } from 'react-redux'
+
+import { toKebabCase } from 'src/utils/toKebabCase'
+
 export const Dendogram = () => {
+  const kebabCasedProjectName = useSelector((state: State) =>
+    toKebabCase(state.project.name)
+  )
+
   return (
     <div>
       <h2> Species dendogram</h2>
       <p>All species recorded in this project.</p>
       <img
-        src={`${process.env.AWS_STORAGE}/dendogram/semifinals_dendogram.svg`}
+        src={`${process.env.AWS_STORAGE}/dendogram/${kebabCasedProjectName}dendogram.svg`}
         alt={'species dendogram'}
         style={{ margin: '20px 0px', backgroundColor: 'white' }}
       />

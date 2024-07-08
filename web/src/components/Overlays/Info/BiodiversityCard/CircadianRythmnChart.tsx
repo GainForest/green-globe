d0a7b55f332ced64a5d4888ec699479f2ca543eb
daviddao <david@gainforest.net>
2024-07-08 18:39:20 -0400

    fixing soundscapes

--- web/src/components/Overlays/Info/BiodiversityCard/CircadianRythmnChart.tsx
+++ web/src/components/Overlays/Info/BiodiversityCard/CircadianRythmnChart.tsx
@@ -2,23 +2,22 @@ import CircularBarChart from 'src/components/CircularBarChart'
 import CircularLineChart from 'src/components/CircularLineChart'
 import CircularMultiLineChartWithCSVLoader from 'src/components/CircularMultiLineChart/CircularMultiLineChart'
 
-export const MaximumCircadianRhythmChart = ({
+export const BarCircadianRhythmChart = ({
   csvPath,
 }: {
   csvPath: string
 }) => {
   return (
     <div>
-      <h2> Maximum Results </h2>
+      <h4>Max values</h4>
       <CircularBarChart csvPath={csvPath} />
     </div>
   )
 }
-
-export const MedianCircadianRhythmChart = ({ csvPath }) => {
+export const LineCircadianRhythmChart = ({ csvPath }) => {
   return (
     <div>
-      <h2>Median Results</h2>
+      <h4>Frequencies</h4>
       <CircularMultiLineChartWithCSVLoader csvPath={csvPath} />
     </div>
   )

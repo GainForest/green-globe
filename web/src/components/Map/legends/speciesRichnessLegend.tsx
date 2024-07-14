511799bfa5eaad225e474c8fc46f2f089b63d21b
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2024-07-15 00:41:37 +0700

    adjust species richness legends and scale

--- web/src/components/Map/legends/speciesRichnessLegend.tsx
+++ web/src/components/Map/legends/speciesRichnessLegend.tsx
@@ -17,36 +17,32 @@ export const SpeciesRichnessLegend = () => {
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`)
 
-    // Set domain exactly from 10 to 50
-    const colorScale = d3
-      .scaleSequential(d3.interpolateViridis)
-      .domain([10, 50])
+    // Set domain from 0 to 12
+    const colorScale = d3.scaleSequential(d3.interpolateViridis).domain([0, 12])
 
     // Color bar
     svg
       .append('g')
       .selectAll('rect')
-      .data(d3.range(10, 50.1, 0.1)) // Use 50.1 to ensure we include 50
+      .data(d3.range(0, 12.01, 0.01)) // Use 12.01 to ensure we include 12
       .enter()
       .append('rect')
-      .attr('x', (d) => (d - 10) * (width / 40))
+      .attr('x', (d) => d * (width / 12))
       .attr('y', 0)
-      .attr('width', width / 400)
+      .attr('width', width / 1200)
       .attr('height', height)
       .style('fill', (d) => colorScale(d))
 
     // Create axis
-    const axisScale = d3.scaleLinear().domain([10, 50]).range([0, width])
-    const axis = d3
-      .axisBottom(axisScale)
-      .tickValues([10, 15, 20, 25, 30, 35, 40, 45, 50])
+    const axisScale = d3.scaleLinear().domain([0, 12]).range([0, width])
+    const axis = d3.axisBottom(axisScale).tickValues([0, 2, 4, 6, 8, 10, 12])
 
     svg.append('g').attr('transform', `translate(0,${height})`).call(axis)
   }, [])
 
   return (
     <div>
-      <h4> Species Richness </h4>
+      <h4>Species Richness</h4>
       <svg ref={ref}></svg>
     </div>
   )
